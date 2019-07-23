327ad49975081017b86500d65557a0e56e8a0944
Gabriela <gabrielahpw@gmail.com>
2019-07-23 16:40:00 -0300

    305

---
+++ lion/static/js/javascript.js
@@ -0,0 +1,9 @@
+let nome = prompt('Digite seu nome')
+let idade = Number(prompt('Digote sua idade'))
+
+if (idade <= 10){
+    alert('você não pode entrar no site')
+}
+else{
+    alert('Bem Vindo')
+}
--- lion/views.py
+++ lion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def pagina_inicial(request):
+    context = {"nome": "joao" , "cachorros" : ["mel", "tobias", "cacau", "bob", "madona", "radija"]}
+    return render(request, 'index.html', context)
